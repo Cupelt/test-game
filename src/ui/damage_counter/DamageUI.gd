d513f507244ddf2fc25975976e5c9d8d8895af8b
Cupelt <57065968+Cupelt@users.noreply.github.com>
2026-04-09 00:02:21 +0900

    2026-04-09

--- src/ui/damage_counter/DamageUI.gd
+++ src/ui/damage_counter/DamageUI.gd
@@ -16,10 +16,14 @@ func init(data: Dictionary):
 	
 func _enter_tree() -> void: # after loaded
 	anim.play("play")
+	# anim.get_animation("play").track_set_key_value(0, 2, randf_range(-wiggle_fector, wiggle_fector))
 	
 	var wiggle_pos = Vector2(randf_range(-wiggle_fector, wiggle_fector), 0)
+	wiggle_body.position = Vector2.ZERO
+	
 	var tween = get_tree().create_tween()
-	tween.tween_property(wiggle_body, "position", wiggle_pos, duration).as_relative()
-	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(wiggle_body, "position", wiggle_pos, duration).as_relative() \
+		.set_ease(Tween.EASE_OUT) \
+		.set_trans(Tween.TRANS_CUBIC)
 	
 	tween.tween_callback(destroy_object)
