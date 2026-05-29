eb92764d04b97be645edab236294dc6ea237453b
Cupelt <57065968+Cupelt@users.noreply.github.com>
2026-05-29 19:38:08 +0900

    2026-05-29

--- src/items/Item.gd
+++ src/items/Item.gd
@@ -1,5 +1,21 @@
-@abstract extends Node2D
+@abstract extends Resource
 class_name Item
 
-var level: float = 0
-@abstract func max_level() -> int
+enum Quality {
+	COMMON,
+	RARE,
+	EPIC,
+	LEGENDARY,
+	MYTHIC
+}
+	
+
+@export var name: String
+@export_multiline var description: String
+@export var icon: Texture2D
+
+func init():
+	pass
+
+func update(delta: float):
+	pass
--- src/items/Weapon.gd
+++ src/items/Weapon.gd
@@ -1,2 +1,7 @@
 @abstract extends Item
 class_name Weapon
+
+@export var ingame_sprite: Texture2D
+@export var max_ammo: int
+
+@export var shot_rate: float
