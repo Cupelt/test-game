b7c1e50e301ecbf244b207fbf82aef1928fbc601
Cupelt <57065968+Cupelt@users.noreply.github.com>
2026-05-30 20:45:14 +0900

    2026-05-30

---
+++ src/items/weapon/Bow_shotgun.gd
@@ -0,0 +1,24 @@
+extends Weapon
+class_name BowShotgun
+
+var arrow_scene = preload("res://scene/projectile/arrow.tscn")
+@export_range(0, 180) var spread_angle: float = 10.0
+
+func attack(player: Player, direction: Vector2):
+	var stats = player.stats
+	
+	var base_angle_rad = direction.angle()
+	
+	for i in range(10):
+		var random_offset_deg = randf_range(-spread_angle / 2.0, spread_angle / 2.0)
+		var random_offset_rad = deg_to_rad(random_offset_deg)
+		
+		var final_angle_rad = base_angle_rad + random_offset_rad
+		var final_direction = Vector2.from_angle(final_angle_rad)
+		
+		ObjectPool.spawn_object(arrow_scene, {
+			"position": player.global_position,
+			
+			"direction": final_direction,
+			"damage": stats.get_stat(EntityStats.StatType.ATTACK) * 3 * MathHelper.randomize_damage_factor(),
+		})
